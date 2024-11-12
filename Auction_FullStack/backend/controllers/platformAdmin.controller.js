ec61030b6712b2760375db21569b0241a4aaf524
saket <saket@saketgupta.com>
2024-11-12 23:58:33 +0530

    Started working on the platform admin controller

--- Auction_FullStack/backend/controllers/platformAdmin.controller.js
+++ Auction_FullStack/backend/controllers/platformAdmin.controller.js
@@ -1,3 +1,56 @@
+// This controller will be performing all the platform admin responsibilities which will operate both on auctioneer and the bidder functions.
+
 import {ApiResponse} from "../utils/ApiResponse.js";
 import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../middlewares/error.middleware.js";
+import mongoose from "mongoose";
+import { Auction } from "../models/auction.model.js";
+import { Paymentproof } from "../models/commissionProof.model.js";
+
+// Platform admin can delete the auction if it finds out to be not that much engaging or of no use as it will help the platform to look clean.
+const deleteAuctionItem=asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new ApiError("Invalid auction id.", 400);
+    }
+    // now fetching the auction details from the database using the id.
+    // if the auction does not exist then it will throw an error.
+    // if the auction exists then it will return the auction details.
+    const auctionItem = await Auction.findById(id);
+    if (!auctionItem) {
+        throw new ApiError("Auction not found.", 404);
+    }
+    // now we will be checking if the auction belongs to the user who is logged in.
+    if (auctionItem.createdBy.toString()!== req.user._id.toString()) {
+        throw new ApiError("You are not authorized to remove this auction.", 403);
+    }
+    // now we will be removing the auction from the database.
+    await auctionItem.deleteOne();
+    return res.status(200).json(new ApiResponse(200, null, "Auction removed successfully"));
+})
+
+const getPaymentproofs=asyncHandler(async(req,res)=>{
+    let paymentproofs=await Paymentproof.find();
+    if(!paymentproofs){
+        throw new ApiError("Payment proofs not found.", 404);
+    }
+    return res.status(200).json(new ApiResponse(200, paymentproofs, "Payment proofs fetched successfully"));
+})
+
+const getpaymentDetails=asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new ApiError("Invalid payment proof id.", 400);
+    }
+    // now fetching the payment proof details from the database using the id.
+    // if the payment proof does not exist then it will throw an error.
+    // if the payment proof exists then it will return the payment proof details.
+    const paymentProof = await Paymentproof.findById(id);
+    if (!paymentProof) {
+        throw new ApiError("Payment proof not found.", 404);
+    }
+    // now we will be checking if the payment proof belongs to the user who is logged in.
+    return res.status(200).json(new ApiResponse(200, paymentProof, "Payment proof"));
+})
+
+export {deleteAuctionItem,getPaymentproofs,getpaymentDetails};
