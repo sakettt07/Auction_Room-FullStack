0a1476bb34bfd77dcabdbd2cd836a7ce4b458449
saket <saket@saketgupta.com>
2024-11-14 23:52:47 +0530

    written two functions for updating the status of the payment proofs

--- Auction_FullStack/backend/controllers/platformAdmin.controller.js
+++ Auction_FullStack/backend/controllers/platformAdmin.controller.js
@@ -1,15 +1,15 @@
 // This controller will be performing all the platform admin responsibilities which will operate both on auctioneer and the bidder functions.
 
-import {ApiResponse} from "../utils/ApiResponse.js";
-import {asyncHandler} from "../utils/asyncHandler.js";
-import {ApiError} from "../middlewares/error.middleware.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../middlewares/error.middleware.js";
 import mongoose from "mongoose";
 import { Auction } from "../models/auction.model.js";
 import { Paymentproof } from "../models/commissionProof.model.js";
 
 // Platform admin can delete the auction if it finds out to be not that much engaging or of no use as it will help the platform to look clean.
-const deleteAuctionItem=asyncHandler(async(req,res)=>{
-    const {id}=req.params;
+const deleteAuctionItem = asyncHandler(async (req, res) => {
+    const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
         throw new ApiError("Invalid auction id.", 400);
     }
@@ -21,7 +21,7 @@ const deleteAuctionItem=asyncHandler(async(req,res)=>{
         throw new ApiError("Auction not found.", 404);
     }
     // now we will be checking if the auction belongs to the user who is logged in.
-    if (auctionItem.createdBy.toString()!== req.user._id.toString()) {
+    if (auctionItem.createdBy.toString() !== req.user._id.toString()) {
         throw new ApiError("You are not authorized to remove this auction.", 403);
     }
     // now we will be removing the auction from the database.
@@ -29,16 +29,16 @@ const deleteAuctionItem=asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200, null, "Auction removed successfully"));
 })
 
-const getPaymentproofs=asyncHandler(async(req,res)=>{
-    let paymentproofs=await Paymentproof.find();
-    if(!paymentproofs){
+const getPaymentproofs = asyncHandler(async (req, res) => {
+    let paymentproofs = await Paymentproof.find();
+    if (!paymentproofs) {
         throw new ApiError("Payment proofs not found.", 404);
     }
     return res.status(200).json(new ApiResponse(200, paymentproofs, "Payment proofs fetched successfully"));
 })
 
-const getpaymentDetails=asyncHandler(async(req,res)=>{
-    const {id}=req.params;
+const getpaymentDetails = asyncHandler(async (req, res) => {
+    const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
         throw new ApiError("Invalid payment proof id.", 400);
     }
@@ -52,5 +52,42 @@ const getpaymentDetails=asyncHandler(async(req,res)=>{
     // now we will be checking if the payment proof belongs to the user who is logged in.
     return res.status(200).json(new ApiResponse(200, paymentProof, "Payment proof"));
 })
+// only  the super admin can change the payment status
+const updateProofStatus = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId(id)) {
+        throw new ApiError("Invalid payment proof id.", 400);
+    }
+    const { amount, status } = req.body;
+    if (!amount || !status) {
+        throw new ApiError("Please provide all the required fields.", 400);
+    }
+    // find the proof
+    let proof = await Paymentproof.findById(id);
+    if (!proof) {
+        throw new ApiError("Payment proof not found.", 404);
+    }
+    proof = await Paymentproof.findByIdAndUpdate(id, {
+        status, amount
+    },
+        { new: true, runValidators: true, useFindAndModify: true})
 
-export {deleteAuctionItem,getPaymentproofs,getpaymentDetails};
+        return res.status(200).json(new ApiResponse(200, { status: status},"Payment proof updated successfully"));
+})
+
+const deletePaymentproof=asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId(id)){
+        throw new ApiError("Invalid payment proof id.", 400);
+    }
+    const proof=Paymentproof.findById(id);
+    if(!proof){
+        throw new ApiError("Payment proof not found.", 404);
+    }
+    await proof.deleteOne();
+    return res.status(200).json(new ApiResponse(200, null,"Payment proof deleted successfully"));
+})
+
+
+
+export { deleteAuctionItem, getPaymentproofs, getpaymentDetails ,updateProofStatus,deletePaymentproof};
