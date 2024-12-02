f8b749dc62b88a4a7e6c8fe59af2c54627c4d124
saket <saket@saketgupta.com>
2024-12-02 23:34:48 +0530

    Nothing just some formatting

--- Auction_FullStack/backend/auto/auctionEndedCron.js
+++ Auction_FullStack/backend/auto/auctionEndedCron.js
@@ -3,7 +3,7 @@ import { Auction } from "../models/auction.model.js";
 import { calculateCommision } from '../controllers/commission.controller.js';;
 import { Bid } from '../models/bid.model.js'
 import { sendEmail } from '../utils/sendEmailFunc.js';
-import ApiError from"../middlewares/error.middleware.js";
+import ApiError from "../middlewares/error.middleware.js";
 
 const auctionEnded = () => {
     cron.schedule("*/1 * * * *", async () => {
@@ -43,14 +43,14 @@ const auctionEnded = () => {
                     const subject = `Congratulations!! You Have won the auction for ${auction.title}`;
                     const message = `Dear ${bidder.userName}, \n\nCongratulations! You have won the auction for ${auction.title}. \n\nBefore proceeding for payment contact your auctioneer via your auctioneer email:${auctioneer.email} \n\nPlease complete your payment using one of the following methods:\n\n1. **Bank Transfer**: \n- Account Name: ${auctioneer.paymentMethods.bankTransfer.bankAccountName} \n- Account Number: ${auctioneer.paymentMethods.bankTransfer.bankAccountNumber} \n- Bank: ${auctioneer.paymentMethods.bankTransfer.bankName}\n\n2. **PhonePe**:\n- You can send payment via PhonePe: ${auctioneer.paymentMethods.stripe.stripeEmail}\n\n3. **PayPal**:\n- Send payment to: ${auctioneer.paymentMethods.paypal.paypalEmail}\n\n4. **Cash on Delivery (COD)**:\n- If you prefer COD, you must pay 20% of the total amount upfront before delivery.\n- To pay the 20% upfront, use any of the above methods.\n- The remaining 80% will be paid upon delivery.\n- If you want to see the condition of your auction item then send your email on this: ${auctioneer.email}\n\nPlease ensure your payment is completed by [Payment Due Date]. Once we confirm the payment, the item will be shipped to you.\n\nThank you for participating!\n\nBest regards,\n Team Auction Walaa`;
                     console.log("SENDING EMAIL TO HIGHEST BIDDER");
-                    sendEmail({email:bidder.email, subject, message})
+                    sendEmail({ email: bidder.email, subject, message })
                     console.log("SUCCESSFULLY EMAIL SEND TO HIGHEST BIDDER");
                 }
-                else{
+                else {
                     await auctioneer.save();
                 }
             } catch (error) {
-                throw new ApiError(error.message|| "Some error in the ended auction cron",400);
+                throw new ApiError(error.message || "Some error in the ended auction cron", 400);
             }
         }
     });
--- Auction_FullStack/backend/routes/bid.routes.js
+++ Auction_FullStack/backend/routes/bid.routes.js
@@ -2,9 +2,9 @@ import express from 'express';
 import { checkRole, isAuthenticated } from '../middlewares/auth.middleware.js';
 import { placebid } from '../controllers/bid.controller.js';
 import { checkAuctiontime } from '../middlewares/auctionEnding.middleware.js';
-const router= express.Router();
+const router = express.Router();
 
-router.route('/placebid/:id').post(isAuthenticated,checkRole('Bidder'),checkAuctiontime,placebid)
+router.route('/placebid/:id').post(isAuthenticated, checkRole('Bidder'), checkAuctiontime, placebid)
 
 
 export default router;
