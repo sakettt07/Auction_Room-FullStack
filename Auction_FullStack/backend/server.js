0be0dbb27d78fe244ba5dcddc9c5e140c2bd97d6
saket <guptasaket898@gmail.com>
2026-03-10 21:22:40 +0530

    fix: update cloudinary import and configuration for consistency

--- Auction_FullStack/backend/server.js
+++ Auction_FullStack/backend/server.js
@@ -1,13 +1,13 @@
-import {app} from "./app.js";
-import cloudinary from "cloudinary";
+import { app } from "./app.js";
+import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.v2.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET,
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT || 4000, () => {
     console.log(`Server listining on PORT ${process.env.PORT}`)
 })
